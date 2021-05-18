692b89b63cc2971c2433f98fb43f981cf6dc4dc9
Michael O'Connor <oconnor97@aol.com>
2021-05-18 11:06:58 -0400

    creating the workout model

---
+++ models/workout.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const { Workout } = require(".");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Type is required"
+
+            },
+
+            name: {
+                type: String,
+                trim: true,
+                required: "Name is required"
+            },
+
+            duration: {
+                type: Number,
+                required: "Duration is required"
+            },
+
+            weight: {
+                type: Number,
+                required: "Weight is required"
+            },
+
+            reps: {
+                type: Number,
+                required: "Reps is required"
+            },
+            sets: {
+                type: Number,
+                required: "Sets are required"
+            }
+
+
+        }
+    ]
+})
+
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;

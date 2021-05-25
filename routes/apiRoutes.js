e368607465cf83cfb75ea9e10c168ce3ed7a452a
Michael O'Connor <oconnor97@aol.com>
2021-05-25 09:43:40 -0400

    trying to display all workouts

---
+++ routes/apiRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+// const Workout = require('../models/workout.js');
+const db = require('../models')
+// Get Workouts
+
+
+router.get('/api/workouts', (req, res) => {
+    db.Workout.find({}).then(workoutDB => {
+        res.json(workoutDB);
+    }).catch(err => {
+        res.json(err.message)
+    })
+});

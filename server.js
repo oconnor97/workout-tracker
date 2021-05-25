fd5458ce080c43117412b9acc6428cd27cb7517c
Michael O'Connor <oconnor97@aol.com>
2021-05-25 09:42:53 -0400

    adding the unifiedTopology to true

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static('public'))
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 app.listen(PORT, () => {

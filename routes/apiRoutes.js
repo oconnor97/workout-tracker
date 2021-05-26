9dea398e43e4f43b7206eaead5d421fce6216a88
Michael O'Connor <oconnor97@aol.com>
2021-05-26 09:12:40 -0400

    changing the get workouts route and adding the totalduration of all exercises

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,13 +4,30 @@ const Workout = require('../models/workout.js');
 // const db = require('../models')
 
 // Get Workouts
+// router.get('/api/workouts', (req, res) => {
+//     Workout.find({}).then(data => {
+//         res.json(data);
+//     }).catch(err => {
+//         res.status(400).json(err)
+//     })
+// });
+
+
 router.get('/api/workouts', (req, res) => {
-    Workout.find({}).then(data => {
-        res.json(data);
-    }).catch(err => {
-        res.status(400).json(err)
-    })
-});
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: '$exercises.duration' }
+            }
+        }
+    ])
+        .then(data => {
+            res.json(data);
+        }).catch(err => {
+            res.status(400).json(err)
+        })
+
+})
 
 // Create Workouts
 router.post('/api/workouts', ({ body }, res) => {

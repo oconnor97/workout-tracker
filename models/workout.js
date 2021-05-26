94499e15dd0e88e4d55c14be71a01382e35fa674
Michael O'Connor <oconnor97@aol.com>
2021-05-26 10:30:37 -0400

    debugging deployment

--- models/workout.js
+++ models/workout.js
@@ -52,6 +52,6 @@ const workoutSchema = new Schema({
 
 
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("workout", workoutSchema);
 
 module.exports = Workout;

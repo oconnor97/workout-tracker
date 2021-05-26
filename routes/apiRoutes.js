035dc8ab28578aef9b417ceca5f596f5505e635e
Michael O'Connor <oconnor97@aol.com>
2021-05-26 09:47:33 -0400

    deploying

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,9 @@
 const router = require('express').Router();
 const Workout = require('../models/workout.js');
 
-// const db = require('../models')
+
 
 // Get Workouts
-// router.get('/api/workouts', (req, res) => {
-//     Workout.find({}).then(data => {
-//         res.json(data);
-//     }).catch(err => {
-//         res.status(400).json(err)
-//     })
-// });
 
 
 router.get('/api/workouts', (req, res) => {
